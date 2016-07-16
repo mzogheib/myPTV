bfd7c1fa970ba5913b59f0c8d761b5975ad13d11
Marwan Zogheib <mzogheib@gmail.com>
2016-07-17 08:06:05 +1000

    ISSUE #13 [gulp] remove whitespaces and newline

--- gulpfile.js
+++ gulpfile.js
@@ -16,8 +16,8 @@ var paths = {
         fonts: 'config/dist/fonts',
         html: 'config/dist/'
     },
-    
-    
+
+
     appFiles: {
         html: 'config/src/index.html',
         js: [
@@ -26,14 +26,14 @@ var paths = {
             'config/src/js/main.js'
         ]
     },
-        
+
     vendorFiles: {
         css: 'config/src/css/slate.min.css',
         fonts: 'config/src/fonts/*',
         js: [
             'config/src/js/libs/hmac-sha1.js',
             'config/src/js/libs/slate.min.js'
-        ]   
+        ]
     },
 
     ftpConfig: 'ftp-config.json'
@@ -65,7 +65,7 @@ gulp.task('build', false, function (cb) {
 });
 
 /**
- * Clean task 
+ * Clean task
  */
 gulp.task('clean', false, function () {
     return del([paths.dest.html]);
@@ -75,7 +75,7 @@ gulp.task('clean', false, function () {
 gulp.task('concatJS', false, function () {
     var app = gulp.src(paths.appFiles.js);
     var vendor = gulp.src(paths.vendorFiles.js);
-    
+
     return merge(app, vendor)
     .pipe(concat('main.js'))
     .pipe(gulp.dest(paths.dest.js));
@@ -87,16 +87,15 @@ gulp.task('concatJS', false, function () {
 gulp.task('copy', false, function () {
     var html = gulp.src(paths.appFiles.html)
         .pipe(gulp.dest(paths.dest.html));
-    
+
     var css = gulp.src(paths.vendorFiles.css)
         .pipe(gulp.dest(paths.dest.css));
-    
+
     var fonts = gulp.src(paths.vendorFiles.fonts)
         .pipe(gulp.dest(paths.dest.fonts));
-          
+
 
     return merge(html, css, fonts);
-        
 });
 
 /**
